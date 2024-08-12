77bed3af43a01664bf6f774bc6e8627a58e87da2
Eduardo Loback <eduardolloback@gmail.com>
2024-08-12 03:24:32 -0300

    fix: script optimization and bug fixes

--- devenv.nix
+++ devenv.nix
@@ -9,17 +9,20 @@ let
   unmountKeyboardBySide = side: ''
     sudo umount ${if side == "left" then left_mount_path else right_mount_path}
   '';
-  generateInstallScriptBySide = side: {
+  installScript = side: ''
+    sudo mount --mkdir ${if side == "left" then left_device_path else right_device_path} ${if side == "left" then left_mount_path else right_mount_path}
+    sudo mv /tmp/adv360pro-firmware/adv360pro_${side}-zmk.uf2 ${if side == "left" then left_mount_path else right_mount_path}
+  '';
+  generateInstallScript = side: {
     exec = ''
-      sudo mount --mkdir ${if side == "left" then left_device_path else right_device_path} ${if side == "left" then left_mount_path else right_mount_path}
-      sudo mv /tmp/adv360pro-firmware/adv360pro_${side}-zmk.uf2 ${if side == "left" then left_mount_path else right_mount_path}
+      ${installScript side}
       read -l -P 'Do you want to continue? [y/N] ' confirm
       switch $confirm
         case Y y
           ${unmountKeyboardBySide side}
           printf "Firmware flashed\n"
           return 0
-        case '''' N n
+        case ''' N n
           return 1
       end
     '';
@@ -33,12 +36,23 @@ in
         ${lib.getExe pkgs.unzip} ${firmware_path} -d ${firmware_out_path}
       '';
     };
-    install_left = generateInstallScriptBySide "left";
-    install_right = generateInstallScriptBySide "right";
+    install_left = generateInstallScript "left";
+    install_right = generateInstallScript "right";
     install_all = {
       exec = ''
         generate_firm
-        install_left && install_right
+        ${installScript "right"}
+        ${installScript "left"}
+        read -l -P 'Do you want to continue? [y/N] ' confirm
+        switch $confirm
+          case Y y
+            ${unmountKeyboardBySide "right"}
+            ${unmountKeyboardBySide "left"}
+            printf "Firmware flashed\n"
+            return 0
+          case ''' N n
+            return 1
+        end
         rm -rf ${firmware_out_path}
       '';
       package = pkgs.fish;
