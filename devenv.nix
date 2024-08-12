57d8de866a1b16b07ee7f2ad70a0ed76c9c268f8
Eduardo Loback <eduardolloback@gmail.com>
2024-08-12 00:31:58 -0300

    fix: script path

--- devenv.nix
+++ devenv.nix
@@ -1,32 +1,47 @@
 { pkgs, lib, ... }:
-
-{
-  scripts.install_firmware = {
+let
+  firmware_path = "~/Downloads/firmware.zip";
+  firmware_out_path = "/tmp/adv360pro-firmware";
+  left_mount_path = "/mnt/left";
+  right_mount_path = "/mnt/right";
+  left_device_path = "/dev/disk/by-id/usb-Adafruit_nRF_UF2_92C5A21B39C4917B-0:0";
+  right_device_path = "/dev/disk/by-id/usb-Adafruit_nRF_UF2_47AB871917154762-0:0";
+  unmountKeyboardBySide = side: ''
+    sudo umount ${if side == "left" then left_mount_path else right_mount_path}
+  '';
+  generateInstallScriptBySide = side: {
     exec = ''
-      ${lib.getExe pkgs.unzip} ~/Downloads/firmware.zip -d /tmp/adv360pro-firmware
-      sudo mount --mkdir /dev/disk/by-id/usb-Adafruit_nRF_UF2_92C5A21B39C4917B-0:0 /mnt/left
-      sudo mount --mkdir /dev/disk/by-id/usb-Adafruit_nRF_UF2_47AB871917154762-0:0 /mnt/right
-      sudo mv /tmp/adv360pro-firmware/adv360pro_left-zmk.uf2 /mnt/left
-      sudo mv /tmp/adv360pro-firmware/adv360pro_right-zmk.uf2 /mnt/right
-      while true
-        read -l -P 'Do you want to continue? [y/N] ' confirm
-        switch $confirm
-          case Y y
-            sudo umount /mnt/left
-            sudo umount /mnt/right
-            return 0
-          case "" N n
-            return 1
-        end
+      sudo mount --mkdir ${if side == "left" then left_device_path else right_device_path} ${if side == "left" then left_mount_path else right_mount_path}
+      sudo mv /tmp/adv360pro-firmware/adv360pro_${side}-zmk.uf2 ${if side == "left" then left_mount_path else right_mount_path}
+      read -l -P 'Do you want to continue? [y/N] ' confirm
+      switch $confirm
+        case Y y
+          ${unmountKeyboardBySide side}
+          printf "Firmware flashed\n"
+          return 0
+        case '''' N n
+          return 1
       end
-      printf "Firmware flashed\n"
     '';
     package = pkgs.fish;
   };
-  scripts.umount_keyboard = {
-    exec = ''
-      sudo umount /mnt/left
-      sudo umount /mnt/right
-    '';
+in
+{
+  scripts = {
+    generate_firm = {
+      exec = ''
+        ${lib.getExe pkgs.unzip} ${firmware_path} -d ${firmware_out_path}
+      '';
+    };
+    install_left = generateInstallScriptBySide "left";
+    install_right = generateInstallScriptBySide "right";
+    install_all = {
+      exec = ''
+        generate_firm
+        install_left && install_right
+        rm -rf ${firmware_out_path}
+      '';
+      package = pkgs.fish;
+    };
   };
 }
